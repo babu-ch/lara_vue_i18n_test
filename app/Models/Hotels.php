33cf1a871c00f95bdc22dcded626ac25e5897849
nane3 <nena3@example.com>
2018-11-05 16:41:43 +0900

    add model

---
+++ app/Models/Hotels.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masahiroohira
+ * Date: 2018/11/05
+ * Time: 16:41
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hotels extends Model
+{
+
+}
