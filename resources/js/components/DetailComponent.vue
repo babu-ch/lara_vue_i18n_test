d50858a88e99409b0df2afc00a2187ea5555c907
nane3 <nena3@example.com>
2018-11-05 18:15:16 +0900

    詳細面

--- resources/js/components/DetailComponent.vue
+++ resources/js/components/DetailComponent.vue
@@ -3,13 +3,26 @@
         <h1>
             これは詳細ページです
         </h1>
+        <h2>{{hotel.name}}</h2>
+        <div>
+            {{hotel.description}}
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+  export default {
+    data() {
+        return {
+          hotel: {}
         }
+    },
+    mounted() {
+      const params = this.$route.params
+      const id = params.id
+      axios.get('/api/detail/' + id).then((response) => {
+        this.hotel = response.data;
+      })
     }
+  }
 </script>
