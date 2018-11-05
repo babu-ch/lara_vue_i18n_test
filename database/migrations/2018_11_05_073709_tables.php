fe9584f3a18269a16dc2af6c3f07c68e5604eb80
nane3 <nena3@example.com>
2018-11-05 16:51:26 +0900

    add prefid

--- database/migrations/2018_11_05_073709_tables.php
+++ database/migrations/2018_11_05_073709_tables.php
@@ -15,6 +15,7 @@ class Tables extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('pref_id');
             $table->string('name');
             $table->string('description');
             $table->timestamps();
