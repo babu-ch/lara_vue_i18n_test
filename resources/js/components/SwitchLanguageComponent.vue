445a987e5ddfaade9f8425e911987c36ac632aef
nane3 <nena3@example.com>
2018-11-06 12:27:16 +0900

    翻訳追加

--- resources/js/components/SwitchLanguageComponent.vue
+++ resources/js/components/SwitchLanguageComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        げんごきりかえ
+        {{$t('message.switch')}}
         <span class="btn btn-primary" @click="langTo('ja')">JP</span>
         <span class="btn btn-primary" @click="langTo('en')">EN</span>
     </div>
@@ -20,6 +20,20 @@
       langTo: function (lang) {
         this.$cookie.set('lang', lang)
         this.$parent.lang = lang
+      }
+    },
+    i18n: {
+      messages: {
+        en: {
+          message: {
+            switch: 'switch locale!'
+          }
+        },
+        ja: {
+          message: {
+            switch: '言語切替！'
+          }
+        }
       }
     }
   };
