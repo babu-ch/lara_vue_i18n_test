075d31e1e563c9afc2bc97b336bf36d80fbae8b6
nena3desuyo@gmail.com <nena3>
2018-11-06 20:50:32 +0900

    assetにしてみる

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,6 @@
         <router-view :lang="lang"></router-view>
     </div>
 </div>
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
