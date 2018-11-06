0fb6ab675bce8a39ec398f7a79582d122ec3c6c9
nane3 <nena3@example.com>
2018-11-06 11:01:41 +0900

    add common translation

---
+++ config/languages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    '/' => [
+        'ja' => [
+            '/' => [
+                'page_description' => 'ここはトップページです',
+                'to_search_page' => '検索画面へ行く！'
+            ]
+        ],
+        'en' => [
+            '/' => [
+                'page_description' => 'Here is Top peeji',
+                'to_search_page' => 'Go to Search page!'
+            ]
+        ]
+    ],
+];
